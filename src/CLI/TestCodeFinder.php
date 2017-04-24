7430a9ae0ecc33c834bcdb066544602054cb5369
Jerome Suarez <jerome.suarez@codingavenue.com>
2017-04-24 16:33:24 +0800

    Issue #12 - Fix double // on the answer path.

--- src/CLI/TestCodeFinder.php
+++ src/CLI/TestCodeFinder.php
@@ -20,9 +20,11 @@ class TestCodeFinder
             throw new \Exception("Cannot find file {$testFile}");
         }
 
-        $answerPath = str_replace($this->baseTestDir, '', $testFile);
+        // This should remove the double // on the resulting path.
+        $separator = DIRECTORY_SEPARATOR;
+        $answerPath = str_replace("{$this->baseTestDir}{$separator}", '', $testFile);
 
-        $answerFile = join(DIRECTORY_SEPARATOR, array($this->baseTestAnswerDir, $answerPath));
+        $answerFile = join($separator, array($this->baseTestAnswerDir, $answerPath));
 
         if (!file_exists($answerFile)) {
             throw new \Exception("Unable to find answer file {$answerFile}");
